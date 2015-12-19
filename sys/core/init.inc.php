91f6cc00270264cbc08b09242bb3a00fe4f1d594
tw7613781 <tw7613781@gmail.com>
2015-12-20 05:51:49 +0900

    resolve fetal problem, cannot found the class board

--- sys/core/init.inc.php
+++ sys/core/init.inc.php
@@ -16,7 +16,7 @@
 	$dbo = new PDO($dsn, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 
 	function __autoload($class){
-		$filename = "../sys/class/class." . $class . ".inc.php";
+		$filename = "../sys/class/class." . strtolower($class) . ".inc.php";
 		if(file_exists($filename)){
 			include_once $filename;
 		}
